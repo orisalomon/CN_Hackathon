c9d32b94debec96b9038f3ba50c7b218c7e47e1d
Dorlevi <dorlevi93@gmail.com>
2021-12-22 20:11:22 +0200

    try to establish connection

--- client.py
+++ client.py
@@ -13,12 +13,10 @@ while True:
 ## connect to server
     data, addr = client_udp.recvfrom(1024)
     break
-print(f"Received offer from {addr}, attempting to connect...")
+print(f"Received offer from {addr[0]}, attempting to connect...")
 
-client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM
-) # TCP Socket
-
-client_tcp.connect((addr[0],5000))
+client_tcp = socket.socket() # TCP Socket
+client_tcp.connect((socket.gethostname(),5000))
 
 # receive data from the server and decoding to get the string.
 # print (s.recv(1024).decode())
@@ -35,43 +33,3 @@ while message.lower().strip() != 'bye':
 
 client_tcp.close()  # close the connection
 
-
-# # close the connection
-# client_socket.close()
-
-
-
-# # # Create a socket object
-# client_socket = socket.socket()		
-
-# # Define the port on which you want to connect
-# port = 12345			
-
-# # connect to the server on local computer
-# client_socket.connect(('192.168.253.1', port))
-
-# # receive data from the server and decoding to get the string.
-# # print (s.recv(1024).decode())
-
-# message = input(" -> ")  # take input
-
-# while message.lower().strip() != 'bye':
-#     client_socket.send(message.encode())  # send message
-#     data = client_socket.recv(1024).decode()  # receive response
-
-#     print('Received from server: ' + data)  # show in terminal
-
-#     message = input(" -> ")  # again take input
-
-# client_socket.close()  # close the connection
-
-
-# # close the connection
-# client_socket.close()
-
-
-# print("Client started, listening for offer requests...")
-# client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-# # Set broadcasting mode
-# client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# client.bind(('', 13117))
--- server.py
+++ server.py
@@ -2,47 +2,22 @@
 import socket
 import config 
 import time              
-
-# import threading
-
-# def set_interval(func,data,to_send, sec):
-#     def func_wrapper(data,to_send):
-#         set_interval(func,data,to_send, sec)
-#         func(data,to_send)
-#         print("Broadcast sent!")
-#     t = threading.Timer(sec, lambda: func_wrapper(data,to_send))
-#     t.start()
-#     return t
-
+import threading
 
 host = socket.gethostbyname(socket.gethostname())
 
+########################## UDP ################################
+
 # Create a UDP socket object
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP Socket   
-
-
-########## TCP bind #############
-# create server tcp socket
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM
-) # TCP Socket
-
-# Get local machine name
-port = config.SERVER_PORT                
-# Reserve a port for your service.
-
-tcp_socket.bind((host, port))        
-# Bind to the port
-
-# maximum of 2 clients
-tcp_socket.listen(2)                 
-
-
-###############################################
 # set broadcast mode
 udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-### Prints Server started ###
-# host = socket.gethostname() 
+def thread_function(func, data, to_send):
+    while True:
+        func(data,to_send)
+        time.sleep(1)
+    
 
 # print out server message
 print(f"Server started, listening on IP address {host}")
@@ -54,25 +29,31 @@ c_addr_lst = []
 string_to_send = b"connect to my server at IP: " + socket.gethostbyname(socket.gethostname()).encode() + b"Port: 5000" 
 # udp_socket.sendto(b"connect to my server at IP: " + socket.gethostbyname(socket.gethostname()).encode() + b"Port: 5000" , ('<broadcast>', 13117))
 
-set_interval(udp_socket.sendto,string_to_send,('<broadcast>', 13117),1)
+udp_thread = threading.Thread(target=thread_function, args=(udp_socket.sendto,string_to_send,('<broadcast>', 13117),))
+udp_thread.start()
+               
+########################## TCP ################################
 
-# time.sleep(1)
+# get the hostname
+host = socket.gethostname()
+port = 5000  # initiate port no above 1024
 
+server_socket = socket.socket()  # get instance
+# look closely. The bind() function takes tuple as argument
+server_socket.bind((host, port))  # bind host address and port together
 
-# Establish connection with client.
-c, addr = tcp_socket.accept()     
+# configure how many client the server can listen simultaneously
+server_socket.listen(2)
+conn, address = server_socket.accept()  # accept new connection
+print("Connection from: " + str(address))
+while True:
+    # receive data stream. it won't accept data packet greater than 1024 bytes
+    data = conn.recv(1024).decode()
+    if not data:
+        # if data is not received break
+        break
+    print("from connected user: " + str(data))
+    data = input(' -> ')
+    conn.send(data.encode())  # send data to the client
 
-# Output the message and Close the connection
-print ('Got connection from', addr)
-c.send('Thank you for connecting'.encode())
-c_addr_lst.append(c,addr)
-
-for i in range(len(c_addr_lst)):
-    c_addr_lst[i][0].send("hello client ".encode() + i.tobytes(2,'big'))
-        
-
-
-c.close()                
-
-# print(f"Server started, listening on IP address {socket.gethostbyname(socket.gethostname())}")
-
+conn.close()  # close the connection
